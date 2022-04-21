bfadd17a5666c777a2957d03ba307880bd51afd2
JulianW051 <1194356@student.roc-nijmegen.nl>
2022-04-21 13:30:08 +0200

    contact front end

--- includes/footer.php
+++ includes/footer.php
@@ -4,6 +4,35 @@
 
         </div>
         <div class="middle">
+            <div class="container3">
+                <div class="registreren">
+                    <div class="title-reserveren">
+                        <h1>
+                            <span>Neem</span>
+                            contact
+                        </h1>
+                    </div>
+                    <form class="form__group field" action="../actions/registreren.php" id="target" method="post">
+                        <div class="input-container">
+                            <input  type="text" name="name" required=""/>
+                            <label>Naam</label>		
+                        </div>
+                        <div class="input-container">
+                            <input type="tel" name="tel" required=""/>
+                            <label>Telefoon nummer</label>
+                        </div>
+                        <div class="input-container">
+                            <input type="email" name="email" required=""/>
+                            <label>Email</label>
+                        </div>
+                        <div class="input-container">
+                            <textarea name="bericht" required=""/></textarea>
+                            <label>Bericht</label>
+                        </div>
+                        <button type="submit">Verstuur</button>
+                    </form>
+                </div>
+            </div>
             <p>Copyright &copy; 2022 Julian Wessels. All Rights Reserved</p>
         </div>
         <div class="right">
